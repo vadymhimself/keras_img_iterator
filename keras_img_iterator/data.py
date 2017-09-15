baf394b6b2888debbb20122d9a423ac3f1e58a12
bolein <bolein95@gmail.com>
2017-09-15 13:23:11 -0500

    initial

---
+++ keras_img_iterator/data.py
@@ -0,0 +1,155 @@
+from keras.preprocessing.image import array_to_img, \
+    img_to_array, load_img, Iterator
+
+import numpy as np
+from six.moves import range
+import os
+
+from keras import backend as K
+
+
+class SingleDirectoryIterator(Iterator):
+    """Iterator capable of reading images from a directory on disk.
+
+    # Arguments
+        directory: Path to the directory to read images from.
+            Each subdirectory in this directory will be
+            considered to contain images from one class,
+            or alternatively you could specify class subdirectories
+            via the `classes` argument.
+        image_data_generator: Instance of `ImageDataGenerator`
+            to use for random transformations and normalization.
+        target_size: tuple of integers, dimensions to resize input images to.
+        color_mode: One of `"rgb"`, `"grayscale"`. Color mode to read images.
+        classes: Optional list of strings, names of sudirectories
+            containing images from each class (e.g. `["dogs", "cats"]`).
+            It will be computed automatically if not set.
+        class_mode: Mode for yielding the targets:
+            `"binary"`: binary targets (if there are only two classes),
+            `"categorical"`: categorical targets,
+            `"sparse"`: integer targets,
+            `"input"`: targets are images identical to input images (mainly
+                used to work with autoencoders),
+            `None`: no targets get yielded (only input images are yielded).
+        batch_size: Integer, size of a batch.
+        shuffle: Boolean, whether to shuffle the data between epochs.
+        seed: Random seed for data shuffling.
+        data_format: String, one of `channels_first`, `channels_last`.
+        save_to_dir: Optional directory where to save the pictures
+            being yielded, in a viewable format. This is useful
+            for visualizing the random transformations being
+            applied, for debugging purposes.
+        save_prefix: String prefix to use for saving sample
+            images (if `save_to_dir` is set).
+        save_format: Format to use for saving sample images
+            (if `save_to_dir` is set).
+    """
+
+    def __init__(self, directory, filenames, labels, image_data_generator,
+                 target_size=(256, 256), color_mode='rgb',
+                 class_mode='categorical',
+                 batch_size=32, shuffle=True, seed=None,
+                 data_format=None,
+                 save_to_dir=None, save_prefix='', save_format='png',
+                 follow_links=False):
+        if data_format is None:
+            data_format = K.image_data_format()
+        self.directory = directory
+        self.image_data_generator = image_data_generator
+        self.target_size = tuple(target_size)
+        if color_mode not in {'rgb', 'grayscale'}:
+            raise ValueError('Invalid color mode:', color_mode,
+                             '; expected "rgb" or "grayscale".')
+        self.color_mode = color_mode
+        self.data_format = data_format
+        if self.color_mode == 'rgb':
+            if self.data_format == 'channels_last':
+                self.image_shape = self.target_size + (3,)
+            else:
+                self.image_shape = (3,) + self.target_size
+        else:
+            if self.data_format == 'channels_last':
+                self.image_shape = self.target_size + (1,)
+            else:
+                self.image_shape = (1,) + self.target_size
+        if class_mode not in {'categorical', 'binary', 'sparse',
+                              'input', None}:
+            raise ValueError('Invalid class_mode:', class_mode,
+                             '; expected one of "categorical", '
+                             '"binary", "sparse", "input"'
+                             ' or None.')
+        self.class_mode = class_mode
+        self.save_to_dir = save_to_dir
+        self.save_prefix = save_prefix
+        self.save_format = save_format
+
+        white_list_formats = {'png', 'jpg', 'jpeg', 'bmp', 'ppm'}
+
+        # first, count the number of samples and classes
+        self.samples = len(filenames)
+        self.filenames = filenames
+
+        classes = set(labels)
+        self.num_class = len(classes)
+        self.class_indices = dict(zip(classes, range(len(classes))))
+        self.classes = list(classes)
+
+        encode = np.vectorize(lambda label: self.class_indices[label])
+        self.labels = encode(labels)
+
+        print('Found %d files belonging to %d classes.' % (
+            self.samples, self.num_class))
+
+        super(SingleDirectoryIterator, self).__init__(self.samples, batch_size,
+                                                      shuffle, seed)
+
+    def next(self):
+        """For python 2.x.
+
+        # Returns
+            The next batch.
+        """
+        with self.lock:
+            index_array, current_index, current_batch_size = next(
+                self.index_generator)
+        # The transformation of images is not under thread lock
+        # so it can be done in parallel
+        batch_x = np.zeros((current_batch_size,) + self.image_shape,
+                           dtype=K.floatx())
+        grayscale = self.color_mode == 'grayscale'
+        # build batch of image data
+        for i, j in enumerate(index_array):
+            fname = self.filenames[j]
+            img = load_img(os.path.join(self.directory, fname),
+                           grayscale=grayscale,
+                           target_size=self.target_size)
+            x = img_to_array(img, data_format=self.data_format)
+            x = self.image_data_generator.random_transform(x)
+            x = self.image_data_generator.standardize(x)
+            batch_x[i] = x
+        # optionally save augmented images to disk for debugging purposes
+        if self.save_to_dir:
+            for i, j in enumerate(index_array):
+                img = array_to_img(batch_x[i], self.data_format, scale=True)
+                fname = '{prefix}_{index}_{hash}_{label}.{format}'.format(
+                    prefix=self.save_prefix,
+                    index=current_index + i,
+                    hash=np.random.randint(1e4),
+                    label=self.classes[self.labels[j]],
+                    format=self.save_format)
+                img.save(os.path.join(self.save_to_dir, fname))
+        # build batch of labels
+        if self.class_mode == 'input':
+            batch_y = batch_x.copy()
+        elif self.class_mode == 'sparse':
+            batch_y = self.labels[index_array]
+        elif self.class_mode == 'binary':
+            batch_y = self.labels[index_array].astype(K.floatx())
+        elif self.class_mode == 'categorical':
+            batch_y = np.zeros((len(batch_x), self.num_class), dtype=K.floatx())
+            for i, label in enumerate(self.labels[index_array]):
+                # convert label number to binary
+                batch_y[i, label] = 1.
+        else:
+            return batch_x
+        return batch_x, batch_y
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='keras_img_iterator',
+    version='1.0',
+    packages=['keras_img_iterator'],
+    url='http://github.com/bolein/keras_img_iterator',
+    license='MIT',
+    author='bolein',
+    author_email='',
+    install_requires=[
+        'keras',
+        'numpy',
+        'six'
+    ],
+    description=''
+)
