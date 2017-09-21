c1ec4adc3cfef08cf95dbdecd30ac55bcf6d974f
bolein <bolein95@gmail.com>
2017-09-21 18:33:54 -0500

    fixed python 3 compatibility

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='keras_img_iterator',
-    version='1.3',
+    version='1.4',
     packages=['keras_img_iterator'],
     url='http://github.com/bolein/keras_img_iterator',
     license='MIT',
